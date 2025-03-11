12189c2a910df48d2264cdcff5535dd6ee022c11
aPumpkn <the1pumpkin.yt@gmail.com>
2025-03-11 17:59:07 -0500

    Update FileType.java
    Added enum type Unknown to differentiate blank files and unsupported files.

--- src/net/pumpkin/fmu/core/file/FileType.java
+++ src/net/pumpkin/fmu/core/file/FileType.java
@@ -1,9 +1,10 @@
 package net.pumpkin.fmu.core.file;
 
-//Representation of a file's extension.
+// Representation of a file's extension.
 public enum FileType {
     
-    FMU,     // Specific for this API
-    UNKNOWN;  // Does not exist or is not compatible
+    FMU,      // Specific for this API
+    BLANK,    // Has no extension
+    UNKNOWN;  // Is not a recognizeable extension
     
 }
