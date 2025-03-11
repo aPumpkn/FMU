81c383dfcf2e2b9bd8d804a2907fb96e94e251c1
aPumpkn <the1pumpkin.yt@gmail.com>
2025-03-10 23:26:20 -0500

    Create FileType.java
    Simple enum for representing file extensions

---
+++ src/net/pumpkin/fmu/core/file/FileType.java
@@ -0,0 +1,9 @@
+package net.pumpkin.fmu.core.file;
+
+//Representation of a file's extension.
+public enum FileType {
+    
+    FMMU,     // Specific for this API
+    UNKNOWN;  // Does not exist or is not compatible
+    
+}
