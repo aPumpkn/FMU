ddb6286710fb205bddcb5809380bc4fc8675069f
aPumpkn <the1pumpkin.yt@gmail.com>
2025-03-16 02:39:36 -0500

    FileType .toString()
    Overwriting the toString method

--- src/net/pumpkin/fmu/core/file/FileType.java
+++ src/net/pumpkin/fmu/core/file/FileType.java
@@ -6,5 +6,25 @@ public enum FileType {
     FMU,      // Specific for this API
     BLANK,    // Has no extension
     UNKNOWN;  // Is not a recognizeable extension
-    
+ 
+    @Override
+    public String toString() {
+        
+        switch (this) {
+            
+            case FMU:
+                return ".fmu";
+                
+            case BLANK:
+                return "";
+                
+            case UNKNOWN:
+                return "Unknown";
+                
+            default:
+                return null;
+            
+        }
+        
+    }
 }
