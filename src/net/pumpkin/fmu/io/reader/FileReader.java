50666a4841871ce9aa44297492e5983cc5891e4d
aPumpkn <the1pumpkin.yt@gmail.com>
2025-03-24 19:43:48 -0500

    Create FileReader.java
    Made an interface for format readers to implement.

---
+++ src/net/pumpkin/fmu/io/reader/FileReader.java
@@ -0,0 +1,40 @@
+package net.pumpkin.fmu.io.reader;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+
+import net.pumpkin.fmu.io.editor.DataContainer;
+
+/*
+ * Responsible for reading the full contents of a file.
+ */
+public interface FileReader {
+    
+    /*
+     * Accesses the file and returns a new DataContainer with all
+     * the retrievable data. 
+     */
+    default DataContainer access(String path) {
+        
+        Map<String,String> map = null;
+        
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get(path))) {
+            
+            map = read(reader);
+            
+        } catch (IOException e) { e.printStackTrace(); }
+        
+        return new DataContainer(map);
+        
+    }
+    
+    /*
+     * Let the Format class implementing this interface determine how 
+     * the file should be read.
+     */
+    Map<String,String> read(BufferedReader reader) throws IOException;
+    
+}
