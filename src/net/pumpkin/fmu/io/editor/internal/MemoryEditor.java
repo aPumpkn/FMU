276f5b0cb47d216ffe44c0206b439e03bdef9d7d
aPumpkn <the1pumpkin.yt@gmail.com>
2025-04-25 04:47:37 -0500

    Update MemoryEditor.java
    Implemented the prescribed changes from DataEditor. The method functionality now depends on the passed DataField option.

--- src/net/pumpkin/fmu/io/editor/internal/MemoryEditor.java
+++ src/net/pumpkin/fmu/io/editor/internal/MemoryEditor.java
@@ -3,6 +3,7 @@ package net.pumpkin.fmu.io.editor.internal;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -14,7 +15,7 @@ import net.pumpkin.fmu.exceptions.FieldFoundException;
 import net.pumpkin.fmu.exceptions.FieldNotFoundException;
 import net.pumpkin.fmu.io.AppFile;
 import net.pumpkin.fmu.io.editor.DataEditor;
-import net.pumpkin.fmu.io.editor.DataType;
+import net.pumpkin.fmu.io.editor.EntryType;
 import net.pumpkin.fmu.io.writer.FmuWriter;
 import net.pumpkin.fmu.io.writer.TextFileWriter;
 import net.pumpkin.fmu.utils.StringUtils;
@@ -42,7 +43,7 @@ public class MemoryEditor implements DataEditor {
     }
 
     @Override
-    public <T> T get(String path, DataType type) {
+    public <T> T get(String path, EntryType type) {
         
         String data = storage.get(StringUtils.stripPath(path));
         String value = "";
@@ -56,7 +57,7 @@ public class MemoryEditor implements DataEditor {
                 
                 if (firstLine) {
                     
-                    if (type != DataType.ofString(line)) 
+                    if (type != EntryType.ofString(line)) 
                         throw new Exception("The passed parameter 'type' does not match the entry's DataType.");
                     
                     firstLine = false;
@@ -74,15 +75,21 @@ public class MemoryEditor implements DataEditor {
     }
 
     @Override
-    public Map<String,String> getEntries(String path) {
+    public Collection<String> collect(String path, DataField fieldType) {
         
         path = StringUtils.stripPath(path);
-        Map<String,String> entries = new LinkedHashMap<>(); 
+        List<String> fields = new LinkedList<>(); 
         int pathLength = path.isEmpty() ? 0 : path.split("/").length;
+        boolean wantsEntry = fieldType.isEntry();
+        boolean wantsRecord = fieldType.isRecord();
         
         for (String key : storage.keySet()) {
             
             String value = storage.get(key);
+            
+            if ((wantsEntry && value == null) || 
+                (wantsRecord && value != null)) continue;
+            
             String[] split = key.split("/");
             int keyLength = split.length;
             String name = split[keyLength - 1];
@@ -92,36 +99,6 @@ public class MemoryEditor implements DataEditor {
                 parent += split[i] + "/";
             
             if (!parent.isEmpty()) parent.substring(0, parent.length() - 1);
-            if (value == null) continue;
-            if (keyLength == pathLength + 1 && key.contains(path)) 
-                entries.put(name, value);
-            
-        }
-        
-        return entries;
-        
-    }
-
-    @Override
-    public List<String> getFields(String path) {
-
-        path = StringUtils.stripPath(path);
-        List<String> fields = new LinkedList<>();
-        int pathLength = path.isEmpty() ? 0 : path.split("/").length; 
-        
-        for (String key : storage.keySet()) {
-            
-            String value = storage.get(key);
-            String[] split = key.split("/");
-            int keyLength = split.length;
-            String name = split[keyLength - 1];
-            String parent = "";
-            
-            for (int i = 0; i < keyLength - 1; i++)
-                parent += split[i] + "/";
-            
-            if (!parent.isEmpty()) parent.substring(0, parent.length() - 1);
-            if (value != null) continue;
             if (keyLength == pathLength + 1 && key.contains(path)) 
                 fields.add(name);
             
@@ -132,22 +109,13 @@ public class MemoryEditor implements DataEditor {
     }
 
     @Override
-    public boolean has(String path) {
-        
-        String value = storage.get(StringUtils.stripPath(path));
-        
-        if (value == null) return false;
-        return true;
-        
-    }
-
-    @Override
-    public boolean hasField(String path) {
+    public boolean has(String path, DataField fieldType) {
         
         String key = StringUtils.stripPath(path);
         String value = storage.get(key);
         
-        if (storage.containsKey(key) && value == null) return true;
+        if (fieldType.isEntry() && value != null) return true;
+        if (fieldType.isRecord() && value == null && storage.containsKey(key)) return true;
         return false;
         
     }
@@ -171,11 +139,11 @@ public class MemoryEditor implements DataEditor {
                     parent += pathing[i] + "/";
                 
                 if (!parent.isEmpty()) parent = parent.substring(0, parent.length() - 1);
-                if (!hasField(parent)) throw new Exception("The parent pathing does not exist.");
+                if (!has(parent, DataField.RECORD)) throw new Exception("The parent pathing does not exist.");
             
             }
             
-            storage.put(path, DataType.asString(value));
+            storage.put(path, EntryType.asString(value));
             
             if (!isChanged) isChanged = true;
             
@@ -187,76 +155,7 @@ public class MemoryEditor implements DataEditor {
     }
 
     @Override
-    public DataEditor rename(String path, String name) {
-
-        try {
-            
-            path = StringUtils.stripPath(path);
-            String value = storage.get(path);
-            
-            if (value == null)
-                throw new FieldNotFoundException();
-
-            storage.remove(path);
-            
-            String[] pathArr = path.split("/");
-            path = "";
-            
-            for (int i = 0; i < pathArr.length - 1; i++)
-                path += pathArr[i] + "/";
-            
-            storage.put(path + name, value);
-            
-            if (!isChanged) isChanged = true;
-            
-        } catch (FieldNotFoundException e) { e.printStackTrace(); }
-        
-        return this;
-        
-    }
-
-    @Override
-    public DataEditor edit(String path, Object value) {
-        
-        try {
-            
-            path = StringUtils.stripPath(path);
-            
-            if (storage.get(path) == null)
-                throw new EntryNotFoundException();
-            
-            storage.put(path, DataType.asString(value));
-            
-            if (!isChanged) isChanged = true;
-            
-        } catch (EntryNotFoundException e) { e.printStackTrace(); }
-        
-        return this;
-        
-    }
-
-    @Override
-    public DataEditor remove(String path) {
-
-        try {
-            
-            path = StringUtils.stripPath(path);
-            
-            if (storage.get(path) == null)
-                throw new EntryNotFoundException();
-            
-            storage.remove(path);
-            
-            if (!isChanged) isChanged = true;
-            
-        } catch (EntryNotFoundException e) { e.printStackTrace(); }
-        
-        return this;
-        
-    }
-
-    @Override
-    public DataEditor addField(String path) {
+    public DataEditor add(String path) {
         
         try {
             
@@ -273,7 +172,8 @@ public class MemoryEditor implements DataEditor {
                 for (int i = 0; i < pathing.length - 1; i++) 
                     parent += pathing[i] + "/";
                 
-                if (!parent.isEmpty() && !hasField(parent)) throw new FieldNotFoundException();
+                if (!parent.isEmpty() && !has(parent, DataField.RECORD)) 
+                    throw new FieldNotFoundException();
                 
             } else throw new Exception("Cannot provide an empty path name.");
             
@@ -284,36 +184,76 @@ public class MemoryEditor implements DataEditor {
         } catch (FieldFoundException e) { e.printStackTrace(); } 
           catch (FieldNotFoundException e) { e.printStackTrace(); } 
           catch (Exception e) { e.printStackTrace(); }
-          return this;
+        
+        return this;
         
     }
 
     @Override
-    public DataEditor addFields(String path) {
+    public DataEditor set(String entry, Object value) {
         
         try {
             
-            path = StringUtils.stripPath(path);
+            entry = StringUtils.stripPath(entry);
             
-            if (storage.containsKey(path) && storage.get(path) == null)
-                throw new FieldFoundException();
-            
-            if (!path.isEmpty()) {
+            if (storage.get(entry) != null) throw new EntryFoundException();
+            if (entry.isEmpty()) throw new IllegalArgumentException("The passed argument 'entry' cannot be a blank string.");
                 
-                String[] pathing = path.split("/");
+            String[] pathing = entry.split("/");
+            
+            if (pathing.length > 1) {
+            
                 String parent = "";
                 
                 for (int i = 0; i < pathing.length - 1; i++) {
                     
                     parent += pathing[i] + "/";
                     
-                    if (!hasField(parent)) storage.put(parent.substring(0, parent.length() - 1), null);
+                    if (!has(parent, DataField.RECORD)) 
+                        storage.put(parent.substring(0, parent.length() - 1), null);
                     
                 }
             
             }
             
-            storage.put(path, null);
+            storage.put(entry, EntryType.asString(value));
+            
+            if (!isChanged) isChanged = true;
+            
+        } catch (EntryFoundException e) { e.printStackTrace(); }
+        
+        return this;
+        
+    }
+
+    @Override
+    public DataEditor set(String entry) {
+        
+        try {
+            
+            entry = StringUtils.stripPath(entry);
+
+            if (entry.isEmpty()) throw new IllegalArgumentException("The passed argument 'entry' cannot be a blank string.");
+            if (storage.containsKey(entry) && storage.get(entry) == null)
+                throw new FieldFoundException();
+            
+            if (!entry.isEmpty()) {
+                
+                String[] pathing = entry.split("/");
+                String parent = "";
+                
+                for (int i = 0; i < pathing.length - 1; i++) {
+                    
+                    parent += pathing[i] + "/";
+                    
+                    if (!has(parent, DataField.RECORD)) 
+                        storage.put(parent.substring(0, parent.length() - 1), null);
+                    
+                }
+            
+            }
+            
+            storage.put(entry, null);
             
             if (!isChanged) isChanged = true;
             
@@ -323,52 +263,83 @@ public class MemoryEditor implements DataEditor {
         return this;
         
     }
-    
+
     @Override
-    public DataEditor renameField(String path, String name) {
+    public DataEditor rename(String field, String name, DataField fieldType) {
 
         try {
             
-            path = StringUtils.stripPath(path);
+            field = StringUtils.stripPath(field);
+            String value = storage.get(field);
             
-            if (!storage.containsKey(path) || storage.get(path) != null)
+            if (fieldType.isEntry() && value == null)
+                throw new EntryNotFoundException();
+            
+            if (fieldType.isRecord() && value != null && storage.containsKey(field))
                 throw new FieldNotFoundException();
 
-            storage.remove(path);
+            storage.remove(field);
             
-            String[] pathArr = path.split("/");
-            path = "";
+            String[] pathArr = field.split("/");
+            field = "";
             
             for (int i = 0; i < pathArr.length - 1; i++)
-                path += pathArr[i] + "/";
-
-            if (!path.isEmpty()) path = path.substring(0, path.length() - 1);
+                field += pathArr[i] + "/";
             
-            storage.put(path + name, null);
+            storage.put(field + name, value);
             
             if (!isChanged) isChanged = true;
             
-        } catch (FieldNotFoundException e) { e.printStackTrace(); }
+        } catch (FieldNotFoundException e) { e.printStackTrace(); } 
+          catch (EntryNotFoundException e) { e.printStackTrace(); }
         
         return this;
         
     }
 
     @Override
-    public DataEditor removeField(String path) {
+    public DataEditor edit(String path, Object value) {
         
         try {
             
             path = StringUtils.stripPath(path);
             
-            if (!storage.containsKey(path) || storage.get(path) != null)
-                throw new FieldNotFoundException();
+            if (storage.get(path) == null)
+                throw new EntryNotFoundException();
             
-            storage.remove(path);
+            storage.put(path, EntryType.asString(value));
             
             if (!isChanged) isChanged = true;
             
-        } catch (FieldNotFoundException e) { e.printStackTrace(); }
+        } catch (EntryNotFoundException e) { e.printStackTrace(); }
+        
+        return this;
+        
+    }
+
+    @Override
+    public DataEditor remove(String field, DataField fieldType) {
+
+        try {
+            
+            field = StringUtils.stripPath(field);
+            
+            if (fieldType.isEntry() && storage.get(field) == null)
+                throw new EntryNotFoundException();
+            
+            if (!storage.containsKey(field) || 
+                (storage.containsKey(field) && storage.get(field) != null))
+                throw new FieldNotFoundException();
+            
+            Map<String,String> queue = new LinkedHashMap<>(storage); 
+            
+            for (String key : queue.keySet()) 
+                if (key.contains(field)) storage.remove(key);
+                
+            if (!isChanged) isChanged = true;
+            
+        } catch (EntryNotFoundException e) { e.printStackTrace(); } 
+          catch (FieldNotFoundException e) { e.printStackTrace(); }
         
         return this;
         
